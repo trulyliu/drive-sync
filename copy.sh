3cf4555f5b770070b12f0d88c2a5dec7d1913644
trulyliu <2002385+trulyliu@users.noreply.github.com>
2021-02-10 15:29:37 +0800

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-rclone copy onedrive: gdrive_mirror:Frank --transfers 48 -v
+#rclone copy onedrive: gdrive_mirror:Frank --transfers 48 -v
+rclone copy dbus-dev:/snis/ dbus-io:/ --transfers 48 -v --drive-server-side-across-configs --fast-list
